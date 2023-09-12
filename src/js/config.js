e65648e209c162051ac003456e2d5d790a68ecdc
velam-varsha <varsha6420@gmail.com>
2023-09-12 23:02:52 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'be9420fc-4da2-44c7-9e66-e29da38263f0';
+export const MODAL_CLOSE_SEC = 2.5;
